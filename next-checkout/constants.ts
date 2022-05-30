70fa949ade4280b61c24f292f15a9603bdaa0c2f
Kirill.Gibalov <http://gitlab.cosmos2.lan/>
2022-05-30 14:35:28 +0300

    git init

---
+++ next-checkout/constants.ts
@@ -0,0 +1,4 @@
+export default {
+    endpoint: 'http://localhost:8004/api',
+    stripe_key: 'pk_test_51L0PumH8xwSs2Pnfs2Ih7Me2fTCQoeWFXJ0nWnyTIBoOOKxgoVj8QpuxjCtWZYNnHwHBjUgNBumB9QLE0cy5uzWN00jNj829ma'
+}
